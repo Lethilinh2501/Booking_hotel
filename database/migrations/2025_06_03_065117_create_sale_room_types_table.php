27ffbf6245dc4de7fe64267a579972706392b619
Ngoc Dung <seehefer@gmail.com>
2025-06-13 16:06:17 +0700

    Saleroom

--- database/migrations/2025_06_03_065117_create_sale_room_types_table.php
+++ database/migrations/2025_06_03_065117_create_sale_room_types_table.php
@@ -4,29 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        // Migration: create_sale_room_types_table.php
         Schema::create('sale_room_types', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('room_type_id');
-            $table->decimal('sale_price', 15, 2);
-            $table->date('start_date');
-            $table->date('end_date');
-            $table->timestamps();
+            $table->string('name'); 
+            $table->decimal('value', 10, 2); 
+            $table->enum('type', ['percent', 'fixed']); 
+            $table->foreignId('room_type_id')->constrained()->onDelete('cascade'); 
+            $table->dateTime('start_date')->nullable(); // 
+            $table->dateTime('end_date')->nullable(); // 
+            $table->enum('status', ['active', 'inactive'])->default('active'); 
+            $table->timestamps(); 
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
+        Schema::table('sale_room_types', function (Blueprint $table) {
+            $table->dropForeign(['room_type_id']);
+        });
+
         Schema::dropIfExists('sale_room_types');
     }
 };
