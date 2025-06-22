67bead5a882763310600fa1c05d2b03ca127eb9a
tuynn1 <tuyenntph44526@fpt.edu.vn>
2025-06-22 21:11:16 +0700

    chỉnh sửa thêm user

--- resources/views/client/layouts/blocks/header.blade.php
+++ resources/views/client/layouts/blocks/header.blade.php
@@ -28,10 +28,11 @@
                             @auth
                                 <a href="#" class="dropdown-toggle" role="button" id="dropdownMenuLink"
                                     data-bs-toggle="dropdown">
-                                    <i class="ri-user-2-fill"></i> Tài khoản
+                                    <i class="ri-user-2-fill"></i> Xin chào {{ auth()->user()->name }}!
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                    {{-- <li><a class="dropdown-item" href="{{ route('profile.edit') }}"><i class="mdi mdi-account"></i> Hồ sơ</a></li>
+                                <li><a class="dropdown-item" href="{{ route('profileUse.edit', auth()->user()->id) }}"><i class="mdi mdi-account"></i> Hồ sơ</a></li>
+                                    {{-- 
                                     <li><a class="dropdown-item" href="{{ route('bookings.index') }}"><i class="ri-contacts-book-line"></i> Đơn đặt phòng</a></li>
                                     <li><a class="dropdown-item" href="{{ route('refunds.lists') }}"><i class="ri-reply-all-line"></i> Lịch sử hoàn tiền</a></li>
                                     <li><a class="dropdown-item" href="{{ route('payments.lists') }}"><i class="ri-bank-card-2-line"></i> Lịch sử giao dịch</a></li> --}}
