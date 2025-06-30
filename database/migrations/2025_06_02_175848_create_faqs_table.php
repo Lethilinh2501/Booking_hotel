d01e36ba5eaca2b07fbe82aa94eff8a9a8323d5c
Ngoc Dung <seehefer@gmail.com>
2025-07-01 02:40:33 +0700

    thêm timestamps và softDeletes

--- database/migrations/2025_06_02_175848_create_faqs_table.php
+++ database/migrations/2025_06_02_175848_create_faqs_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
             $table->text('answer')->nullable();
             $table->string('image')->nullable();
             $table->boolean('is_active')->default(true);
+            $table->softDeletes(); 
+            $table->timestamps();         
         });
     }
 
