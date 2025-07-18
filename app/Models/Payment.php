0091408638c951b7c0d264762bef02705037c611
Lethilinh2501 <your_email@example.com>
2025-07-18 21:22:13 +0700

    sửa lại db payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = ['booking_id', 'amount', 'method', 'status'];
+    protected $fillable = [
+        'user_id',
+        'method',
+        'amount',
+        'is_partial',
+        'status',
+        'transaction_id',
+        'booking_id',
+    ];
+
+    // Quan hệ
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function booking()
     {
--- app/Models/PaymentSetting.php
+++ app/Models/PaymentSetting.php
@@ -5,10 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// Model: PaymentSetting.php
 class PaymentSetting extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['gateway', 'is_enabled', 'config'];
+    protected $fillable = ['deposit_percentage'];
 }
